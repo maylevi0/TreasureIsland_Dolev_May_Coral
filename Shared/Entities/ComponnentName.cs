908820ac15bb14f943a5265c87dd22860b98bfcb
maylevi0 <86535554+maylevi0@users.noreply.github.com>
2022-08-15 12:37:30 +0300

    15.08.2022

---
+++ Shared/Entities/ComponnentName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreasureIsland_Dolev_May_Coral.Shared.Entities
+{
+    class ComponnentName
+    {
+    }
+}
