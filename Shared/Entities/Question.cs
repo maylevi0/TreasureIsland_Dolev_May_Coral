256b6096f75f0bc837e2ff68c7b4f73eec5c33fb
Dolev1699 <75645737+Dolev1699@users.noreply.github.com>
2022-10-24 10:31:07 +0300

    a
    a

--- Shared/Entities/Question.cs
+++ Shared/Entities/Question.cs
@@ -15,7 +15,7 @@ namespace TreasureIsland_Dolev_May_Coral.Shared.Entities
         public string QuestionTitle { get; set; }
         public string QuestionImage { get; set; }
         public int GameID { get; set; }
-        public Game QuestionGame { get; set; }
+        public Game QuestionGame { get; set; }       
         public List<Distractor> QuestionDistractors { get; set; }
 
     }
