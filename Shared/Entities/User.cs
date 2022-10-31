ac137811b17c6442cfc4316d257c6e70355abd26
Dolev1699 <75645737+Dolev1699@users.noreply.github.com>
2022-10-31 23:43:02 +0200

    3110
    3110

--- Shared/Entities/User.cs
+++ Shared/Entities/User.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace TreasureIsland_Dolev_May_Coral.Shared.Entities
 {
     public class User
     {
         public int ID { get; set; }
+        
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
